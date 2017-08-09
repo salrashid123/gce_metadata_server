7eeb720aad923e59d99cd96189f69947d5c39392
salrashid123 <salrashid123@gmail.com>
2017-08-08 21:51:14 -0700

    wrapper->subprocess

--- gcloud_wrapper.py
+++ gcloud_wrapper.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python
-##############
-#
-# gcloud CLI wrapper - without re-invoking Python
-#
-# author: rensin@
-#
-##############
 
 import os
 import sys
 import string
-
-# need to setup a few path items so the modules find the right
-# bits
+import subprocess
 
 GCLOUD_PATH = None
 
@@ -26,52 +17,9 @@ if GCLOUD_PATH == None:
 	print "The gcloud command must be in your PATH someplace for this to work"
 	exit(-1)
  
-if os.path.islink(GCLOUD_PATH):
-    GCLOUD_PATH = os.path.realpath(GCLOUD_PATH)
-
-SDK_PATH = os.path.dirname(os.path.dirname(GCLOUD_PATH)) + '/lib/'
-
-sys.path.insert(0, SDK_PATH)
-
-_GCLOUD_PY_DIR = os.path.dirname(SDK_PATH)
-_THIRD_PARTY_DIR = os.path.join(_GCLOUD_PY_DIR, 'third_party')
-
-if os.path.isdir(_THIRD_PARTY_DIR):
-  sys.path.insert(0, _THIRD_PARTY_DIR)
-
-# the gcloud code captures STDOUT as soon as the gcloud_main
-# module is loaded. So... If we want to be able to work with
-# the output of the gcloud command, we have to capture STDOUT
-# first!
-
-from StringIO import StringIO
-
-STDOUT = sys.stdout         # save the original STDOUT
-GCLOUD_OUTPUT = StringIO()  # create a new file object
-sys.stdout = GCLOUD_OUTPUT  # set stdout to the new object
-
-# load the key module. it will capture what
-# it thinks is stdout, but is really the new
-# file object we setup.
-
-import googlecloudsdk.gcloud_main
-
-sys.stdout = STDOUT # restore the old stdout so 'print' works
-
-# gcloud exports a VERY handy object named CLI
-# that does 99% of our heavy lifting. Yay!
-# 
-# So let's grab an instance of that and keep 
-# it around for our use
-
-_CLI = googlecloudsdk.gcloud_main.CreateCLI([])
-
-# This is a little wrapper function that invokes the cli
-# with your arguments and then returns the output
-# as a simple string
 
 def GCloud(_args):
-	_CLI.Execute(args=_args)
-	output = GCLOUD_OUTPUT.getvalue()
-	GCLOUD_OUTPUT.truncate(0);
-	return output
+  args = ['gcloud'] + _args
+  process = subprocess.Popen(args, stdout=subprocess.PIPE)
+  out, err = process.communicate()
+  return out
